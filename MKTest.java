5bc0bf847a2dbc58bb31db9f67c28857c5565d95
Joachim Meyer-Kayser <meyer-kayser@web.de>
2025-01-15 14:31:46 +0100

    New class MKtest added.

---
+++ MKTest.java
@@ -0,0 +1,14 @@
+
+/**
+ * Beschreiben Sie hier die Klasse MKTest.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class MKTest
+{
+    public MKTest()
+    {
+        System.out.println("MKTest generated.");
+    }
+}
